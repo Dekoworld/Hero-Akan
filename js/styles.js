8a7b2eaa3c4336b40c4c32b122e48a530497563d
Dekoworld <karituderrick@gmail.com>
2019-09-21 08:26:24 +0300

    programming the linked js to accomplish page purpose,styling using the custom stylesheet

---
+++ js/styles.js
@@ -0,0 +1,12 @@
+var cc = ["18","19","20"];
+var day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var femaleakan = 
+
+
+
+function akan (){
+    var year = parseInt(document.getElementById("Year1"), Value);
+    var month = parseInt(document.getElementById("Month1"),Value);
+    var date = parseInt(document.getElementById("Date1").Value);
+    
+}
