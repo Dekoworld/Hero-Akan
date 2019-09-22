25c2a809f9bc13a22e4a1eecbfcf78a1f9c6e6d6
Dekoworld <karituderrick@gmail.com>
2019-09-22 21:50:47 +0300

    added files and an images folder

--- js/styles.js
+++ js/styles.js
@@ -1,39 +1,28 @@
-
-var day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+//Initialize Days of Week Array
+var days = ['SUNDAY','MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY'];
+//Initialize Akan names Array 
 var male = ["Kwasi","Kwandwo","Kwabena","Kwaku","Yaw"," Kofi"," Kwame"];
+//Initialize Akan names Array
 var female = ["Akosua"," Adwoa","Abenaa"," Akua","Yaa","Afua","Ama"];
-
-function AkanName (){
-    var Year1 = parseInt(document.getElementById("Year1").value);
-    if(Year1 < 1900) {
-        alert("Please,enter range(1900-present)!");
-     }
-    var Month1 = parseInt(document.getElementById("Month1").value);
- if{
-    ((Month1 < 0) || (Month1 > 12)) {
-       alert("Month not valid!")..);
-    }
-    var Date1 = parseInt(document.getElementById("Date1").value);
-   
-if((Date1 < 0) || (Date1 > 31)) {
-        alert("Date not valid!");
-    }
-  
-
-    var day =new day(Date1 +"-"+Month1 +"-"+Year1);
-    var bd = day.getdate1();
-    var AkanName;
-  
-
- var gender = document.getElementById("gender"}.value};
- function getgender(){
-     return gender;
-        }
-    if (gender==="male"){
-        AkanName = maleName(bd);
-    }
-    else{
-        AkanName = femaleName(bd);
-    }
-    alert("Your day of birth is" +day[bd]+"and you AKAN name is"+AkanName);
+//Initialize Months Week Array 
+var monthNames = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+                      
+var today = new Date();
+var dd   = today.getDate();
+var mm   = monthNames[today.getMonth()]; 
+var yyyy = today.getFullYear();
+var day  = days[today.getDay()];
+today = 'Date is :' + dd + '-' + mm + '-' + yyyy;
+document.write(today +"<br>");
+document.write('Day is : ' + day );
 }
+currentDate()
+var val4 = parseInt(((val2x+1)*3)/5, 10)
+var val5 = parseInt(val3/4, 10)
+var val6 = parseInt(val3/100, 10)
+var val7 = parseInt(val3/400, 10)
+var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
+var val9 = parseInt(val8/7, 10)
+var val0 = val8-(val9*7)
+form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
+form.result2.value = days[val0]
